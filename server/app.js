fc6b667a8949e01cfa3d35ea020457dd5a49c0d7
Paul Mann <paulwmcs2003@gmail.com>
2025-02-04 10:47:57 -0500

    Added ability to authenticate with server and basic client side signup page

--- server/app.js
+++ server/app.js
@@ -1,20 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const { createClient } = require('@supabase/supabase-js')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Supabase Setup
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-var app = express();
+//Routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+const app = express();
+
+//IDK what this does but don't delete it
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
