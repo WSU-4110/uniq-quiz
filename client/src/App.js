7520d5ef0a532fdb8391b99674381fda78bca29c
Paul Mann <paulwmcs2003@gmail.com>
2025-02-09 22:39:26 -0500

    Added front-end login and signup pages with stylesheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import axios from 'axios';
 import Home from "./components/Home";
-import Signup from './components/Signup';
+import Signup from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 function App() {
   return (
     <BrowserRouter>
-        <div>
+        <div className="ALCOHOL">
             <nav>
                 <Link to="/">Home</Link> | {' '}
-                <Link to="/signup">Signup</Link>
+                <Link to="/signup">Signup</Link> | {' '}
+                <Link to="/login">Login</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </div>
     </BrowserRouter>
