cacc99c52548418493b43283660edc52621cbc85
Trevor-D-H <hi5785@wayne.edu>
2025-02-18 14:55:30 -0500

    Trying to merge main into HomePage by resetting

--- client/src/pages/Auth/Game/Host.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function UserSettings() {
-    return (
-        <h1>WIP - host.jsx</h1>
-    )
-}
-
-export default UserSettings;
--- client/src/pages/Auth/Game/Join.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Join() {
-    return (
-        <h1>WIP - Join.jsx</h1>
-    )
-}
-
-export default Join;
